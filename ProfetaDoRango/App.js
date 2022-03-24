288c43f14f4b8d9ab58f3845bd504eb4cb66f93d
Sophia M Silveira <sophiasilveira2003@gmail.com>
2022-03-24 19:42:18 -0300

    'config'

--- ProfetaDoRango/App.js
+++ ProfetaDoRango/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
