ea48713da34d709d7f638a3916806a0772b057dc
Sophia M Silveira <sophiasilveira2003@gmail.com>
2022-03-25 18:18:54 -0300

    'finishedTwoOfTen'

--- ProfetaDoRango/src/pages/UserRegister/index.js
+++ ProfetaDoRango/src/pages/UserRegister/index.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import {View,
-        Text,
-        StyleSheet,
-        TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList, Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import {openDatabase} from 'react-native-sqlite-storage';
 
+const db = openDatabase({name: 'app_db.db', createFromLocation: 1});
 
-function UserRegister({navigation}) {
+const UserRegister = () => {
+  let [flatListItems, setFlatListItems] = useState([]);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql('SELECT * FROM tbl_user',
+      [],
+      (tx, results) => {
+        var temp = [];
+        for (let i = 0; i < results.rows.length; ++i)
+          temp.push(results.rows.item(i));
+        setFlatListItems(temp);
+      });
+    });
+  }, []);
+
+  let listViewItemSeparator = () => {
+    return (
+      <View style={{height: 0.2, width: '100%', backgroundColor: '#808080'}} />
+    );
+  };
+
+  let listItemView = (item) => {
+    return (
+      <View
+        key={item.user_id}
+        style={styles.list}>
+        <Text>Id: {item.user_id}</Text>
+        <Text>Name: {item.user_name}</Text>
+        <Text>Senha: {item.user_password}</Text>
+        <Text>e-mail: {item.user_email}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style = {styles.container}>
-      <Text>User Register</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View>
+          <FlatList
+            data={flatListItems}
+            ItemSeparatorComponent={listViewItemSeparator}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item}) => listItemView(item)}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: "#000",
     },
-  });
+    list: {
+     backgroundColor: "pink",
+    },
+});
 
 export default UserRegister;
--- ProfetaDoRango/src/pages/UserUpdate/index.js
+++ ProfetaDoRango/src/pages/UserUpdate/index.js
@@ -1,17 +1,156 @@
-import React from 'react';
-import {View,
-        Text,
-        StyleSheet,
-        TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Alert,
+  SafeAreaView,
+  Text,
+  StyleSheet
+} from 'react-native';
 
-function UserUpdate({navigation}) {
+import Mytextinput from '../../components/Mytextinput';
+import Mybutton from '../../components/Mybutton';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+const db = openDatabase({name: 'app_db.db', createFromLocation: 1});
+
+
+
+const UserUpdate = ({navigation}) => {
+ let [inputUserId, setInputUserId] = useState('');
+  let [userName, setUserName] = useState('');
+  let [userContact, setUserContact] = useState('');
+  let [userAddress, setUserAddress] = useState('');
+
+  let updateAllStates = (name, contact, address) => {
+    setUserName(name);
+    setUserContact(contact);
+    setUserAddress(address);
+  };
+
+  let searchUser = () => {
+    console.log(inputUserId);
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM tbl_user where user_name = ?',
+        [inputUserId],
+        (tx, results) => {
+          var len = results.rows.length;
+          if (len > 0) {
+            let res = results.rows.item(0);
+            updateAllStates(res.user_name, res.user_id, res.user_email);
+          } else {
+            alert('No user found');
+            updateAllStates('', '', '');
+          }
+        },
+      );
+    });
+  };
+  let updateUser = () => {
+    console.log(inputUserId, userName, userContact, userAddress);
+
+    if (!inputUserId) {
+      alert('Please fill User id');
+      return;
+    }
+    if (!userName) {
+      alert('Please fill name');
+      return;
+    }
+    if (!userContact) {
+      alert('Please fill Contact Number');
+      return;
+    }
+    if (!userAddress) {
+      alert('Please fill Address');
+      return;
+    }
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        'UPDATE tbl_user set user_name=?, user_id=?, user_password=? where user_name=?',
+        [userName, userContact, userAddress, inputUserId],
+        (tx, results) => {
+          console.log('Results', results.rowsAffected);
+          if (results.rowsAffected > 0) {
+            Alert.alert(
+              'Success',
+              'User updated successfully',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () => navigation.navigate('Home'),
+                },
+              ],
+              {cancelable: false},
+            );
+          } else alert('Updation Failed');
+        },
+      );
+    });
+  };
+
   return (
-    <View style = {styles.container}>
-      <Text>User Update</Text>
-    </View>
-  );
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1}}>
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <KeyboardAvoidingView
+              behavior="padding"
+              style={{flex: 1, justifyContent: 'space-between'}}>
+              <Mytextinput
+                placeholder="Enter User Id"
+                style={{padding: 10}}
+                onChangeText={
+                  (inputUserId) => setInputUserId(inputUserId)
+                }
+              />
+              <Mybutton
+                title="Search User"
+                customClick={searchUser}
+              />
+              <Mytextinput
+                placeholder="Enter Name"
+                value={userName}
+                style={{padding: 10}}
+                onChangeText={
+                  (userName) => setUserName(userName)
+                }
+              />
+              <Mytextinput
+                placeholder="Enter Contact No"
+                value={'' + userContact}
+                onChangeText={
+                  (userContact) => setUserContact(userContact)
+                }
+                maxLength={10}
+                style={{padding: 10}}
+                 keyboardType="numeric"
+              />
+              <Mytextinput
+                value={userAddress}
+                placeholder="Enter Address"
+                onChangeText={
+                  (userAddress) => setUserAddress(userAddress)
+                }
 
+              />
+              <Mybutton
+                title="Update User"
+                customClick={updateUser}
+              />
+            </KeyboardAvoidingView>
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
+
+
+
 
 const styles = StyleSheet.create({
     container: {
