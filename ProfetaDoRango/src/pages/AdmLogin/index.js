b7bc55c60f4f729a96e420861f871800c93bbf40
Sophia M Silveira <sophiasilveira2003@gmail.com>
2022-03-25 21:28:12 -0300

    'FourOfTenBD'

--- ProfetaDoRango/src/pages/AdmLogin/index.js
+++ ProfetaDoRango/src/pages/AdmLogin/index.js
@@ -1,30 +1,102 @@
-import React from 'react';
-import {View,
-        Text,
-        StyleSheet,
-        TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Alert,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+import Mytextinput from '../../components/Mytextinput';
+import Mybutton from '../../components/Mybutton';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+const db = openDatabase({name: 'app_db.db', createFromLocation: 1});
+
+const AdmLogin = ({navigation}) => {
+  let [inputAdmName, setInputAdmName] = useState('');
+  let [inputAdmPassword, setInputAdmPassword] = useState('');
+  let [admName, setAdmName] = useState('');
+  let [admPassword, setAdmPassword] = useState('');
+
+  let updateAllStates = (name, password) => {
+    setAdmName(name);
+    setAdmPassword(password);
+  };
+
+  let searchUser = () => {
+    console.log(inputAdmName, inputAdmPassword);
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM tbl_adm where (adm_name, adm_password) = (?,?)',
+        [inputAdmName, inputAdmPassword],
+        (tx, results) => {
+          var len = results.rows.length;
+          if (len > 0) {
+            let res = results.rows.item(0);
+            updateAllStates(res.adm_name, res.adm_password);
+            navigation.navigate('Home');
+          } else {
+            alert('No user found');
+            updateAllStates('', '', '');
+          }
+        },
+      );
+    });
+  };
 
 
-function AdmLogin({navigation}) {
   return (
-    <View style = {styles.container}>
-      <Text>AdmLogin</Text>
-      <TouchableOpacity
-        onPress = { () =>
-          navigation.navigate('AdmHome')}>
-           <Text>Ir AdmHome</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <KeyboardAvoidingView
+              behavior="padding"
+              style={styles.keyboard}>
+              <Mytextinput
+                placeholder="Enter Name"
+                style={styles.padding}
+                onChangeText={
+                  (inputAdmName) => setInputAdmName(inputAdmName)
+                }
+              />
+              <Mytextinput
+                placeholder="Enter Password"
+                style={styles.padding}
+                onChangeText={
+                  (inputAdmPassword) => setInputAdmPassword(inputAdmPassword)
+                  }
+               />
+              <Mybutton
+                title="Search User"
+                customClick={searchUser}
+              />
+            </KeyboardAvoidingView>
+          </ScrollView>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-
 };
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
     },
+
+    keyboard:{
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+
+    padding: {
+      padding: 10,
+    },
+
   });
 
 export default AdmLogin;
--- ProfetaDoRango/src/pages/UserLogin/index.js
+++ ProfetaDoRango/src/pages/UserLogin/index.js
@@ -1,34 +1,107 @@
-import React from 'react';
-import {View,
-        Text,
-        StyleSheet,
-        TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Alert,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+import Mytextinput from '../../components/Mytextinput';
+import Mybutton from '../../components/Mybutton';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+const db = openDatabase({name: 'app_db.db', createFromLocation: 1});
+
+const AdmLogin = ({navigation}) => {
+  let [inputUserName, setInputUserName] = useState('');
+  let [inputUserPassword, setInputUserPassword] = useState('');
+  let [userName, setUserName] = useState('');
+  let [userPassword, setUserPassword] = useState('');
+
+  let updateAllStates = (name, password) => {
+    setUserName(name);
+    setUserPassword(password);
+  };
+
+  let searchUser = () => {
+    console.log(inputUserName, inputUserPassword);
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM tbl_user where (user_name, user_password) = (?,?)',
+        [inputUserName, inputUserPassword],
+        (tx, results) => {
+          var len = results.rows.length;
+          if (len > 0) {
+            let res = results.rows.item(0);
+            updateAllStates(res.user_name, res.user_password);
+            navigation.navigate('Home');
+          } else {
+            alert('No user found');
+            updateAllStates('', '', '');
+          }
+        },
+      );
+    });
+  };
 
 
-function UserLogin({navigation}) {
   return (
-    <View style = {styles.container}>
-      <Text>User Login</Text>
-      <TouchableOpacity
-       onPress = { () =>
-        navigation.navigate('Home')}>
-        <Text>Ir Home</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress = { () =>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <KeyboardAvoidingView
+              behavior="padding"
+              style={styles.keyboard}>
+              <Mytextinput
+                placeholder="Enter Name"
+                style={styles.padding}
+                onChangeText={
+                  (inputUserName) => setInputUserName(inputUserName)
+                }
+              />
+              <Mytextinput
+                placeholder="Enter Password"
+                style={{padding: 10}}
+                onChangeText={
+                  (inputUserPassword) => setInputUserPassword(inputUserPassword)
+                  }
+               />
+              <Mybutton
+                title="Search User"
+                customClick={searchUser}
+              />
+            </KeyboardAvoidingView>
+          </ScrollView>
+        </View>
+        <TouchableOpacity
+          onPress = { () =>
           navigation.navigate('AdmLogin')}>
-           <Text>Ir AdmLogin</Text>
-      </TouchableOpacity>
-    </View>
+            <Text>Ir AdmLogin</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
     },
-  });
 
-export default UserLogin;
+    keyboard:{
+      flex: 1,
+      justifyContent: 'space-between',
+    },
+
+    padding: {
+      padding: 10,
+    },
+
+});
+
+export default AdmLogin;
